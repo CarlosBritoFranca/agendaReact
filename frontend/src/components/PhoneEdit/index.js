90b56acdefb4dfbe20a2ada016c9c08530c867ba
CarlosBritoFranca <consult.franca@gmail.com>
2020-04-20 16:58:03 -0300

    ajustes

--- frontend/src/components/PhoneEdit/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { Modal } from "react-bootstrap";
-// import { Container } from './styles';
-
-export default function PhoneEdit({ setModalPhoneEdit, ModalPhoneEdit }) {
-  return <></>;
-}
